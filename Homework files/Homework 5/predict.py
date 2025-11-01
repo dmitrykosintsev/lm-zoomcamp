3fb06dff9cdfcb03c0e7d1f705209e352e436472
dmitrykosintsev <DmitriiKosintsev@my.uopeople.edu>
2025-11-01 23:54:17 +0800

    Unit 5 homework

---
+++ Homework files/Homework 5/predict.py
@@ -0,0 +1,14 @@
+# Version 1: using the code directly
+import pickle
+
+with open('pipeline_v1.bin', 'rb') as f_in:
+    pipeline = pickle.load(f_in)
+
+customer = {
+    "lead_source": "paid_ads",
+    "number_of_courses_viewed": 2,
+    "annual_income": 79276.0
+}
+
+lead_score = pipeline.predict_proba(customer)[0, 1]
+print(lead_score)
